d8bdb947ae34c13939af29de3b008f1645e100be
nate <noubre@gmail.com>
2023-08-24 00:03:05 -0700

    Updates

--- code-ch01/ecc.py
+++ code-ch01/ecc.py
@@ -23,7 +23,7 @@ class FieldElement:
 
     def __ne__(self, other):
         # this should be the inverse of the == operator
-        raise NotImplementedError
+        return not (self == other)
 
     # tag::source2[]
     def __add__(self, other):
@@ -36,10 +36,12 @@ class FieldElement:
     def __sub__(self, other):
         if self.prime != other.prime:
             raise TypeError('Cannot subtract two numbers in different Fields')
+        num = (self.num - other.num) % self.prime
+        return self.__class__(num, self.prime)
         # self.num and other.num are the actual values
         # self.prime is what we need to mod against
         # We return an element of the same class
-        raise NotImplementedError
+        
 
     def __mul__(self, other):
         if self.prime != other.prime:
